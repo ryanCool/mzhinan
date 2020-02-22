588c0a0c75f897af210c985d6f7ef8b6b56b69e4
ryanCool <rexq4@hotmail.com>
2020-02-23 00:15:08 +0800

    Wip

---
+++ main.go
@@ -0,0 +1,241 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+const token = "8F3E6788-7DA2-3F7D-4C11-7F25348E8713"
+
+var finalData = map[string][]RequireData{}
+
+var citymap = map[string]string{
+	"6B70D2A3-8061-F37D-73DB-355134A0521F": "上海",
+	"8D553380-2180-440A-8E3E-6F926CCB77F6": "北京",
+	"834FA3FA-1B9F-4771-B43B-A43F3FE940CC": "臺灣",
+	"ECCFF058-9506-4187-536C-EE8A39B5D79D": "香港",
+	"38E469DD-34B7-347A-9C77-D0B1F326B0C5": "澳門",
+	"E9E7F1CC-2414-4FB1-AC40-282424CC36ED": "廣州",
+}
+
+var catMap = map[string]string{
+	"A69483F9-8753-D289-22DA-166AE175D7DF": "粵菜",
+	"04E94EFB-ED3A-FFD1-F9F3-D2D563767D42": "法國菜",
+	"1A502C9D-1CC4-E6AF-A6F3-3DB04971B3F5": "時尚歐陸菜",
+	"209258ED-77C7-E705-0DC3-F08CC3F2CDF5": "義大利菜",
+	"487110D6-671C-5842-BC22-8409BB78E032": "瑞士",
+	"A29EF03F-7E91-F075-DBDD-C5360F6427DA": "時尚法國菜",
+	"BA2BC26C-A4FA-4295-80E0-F94A97ACBFF6": "祕魯菜",
+	"CC92D10A-9201-4226-9151-14569F510EEE": "北歐菜",
+	"D928DAC2-637C-5D24-D450-13E133B83767": "扒房",
+	"F3A5E624-BD7B-4DC7-97B6-A163B329CE64": "餃子",
+	"FD7625CA-C216-6873-D5DC-10338D2AC325": "美國義大利菜",
+	"121FB7A3-2B22-4B56-B32C-28881CCDCFE9": "落林菜",
+	"80EA99CA-38DD-403A-8B5F-2361EFA293A9": "里昂菜",
+	"A8D610F0-E4C7-4E85-900A-5FF6189A24E7": "海鮮",
+	"B2F05785-CFB1-46BD-984A-5C14D540A275": "依善菜",
+	"F46081D9-7344-7562-EEDA-02F59C8F61EB": "海鮮",
+	"03C659A1-60DE-EB53-E647-81608C5D4227": "臺灣菜",
+}
+
+var scoreMap = map[string]string{
+	"0590FDA9-9DC0-4ACE-BFEA-83A57FDFB15F": "三星",
+	"DC960ECC-E15B-46E2-89F5-B44EC31743F1": "二星",
+	"D2CC5438-B44C-436B-9D9C-4BEC4564CE79": "一星",
+	"DE23F387-79CE-4992-9398-697AE70F017C": "必比登",
+	"D87A87B3-782B-4C02-83FE-C0467D934923": "餐盤",
+	"58B8EF0B-024A-4581-831B-0B73AA35058D": "街頭美食",
+}
+
+func getCityMap(config Config) {
+	for _, cuisine := range config.Cuisines {
+		catMap[cuisine.ID] = cuisine.Name
+
+	}
+}
+
+func getCatMap(config Config) {
+	for _, location := range config.Location {
+		citymap[location.ID] = location.NameZh
+	}
+}
+
+func getConfig() {
+
+	url := "https://api.mzhinan.cn/api/"
+	method := "POST"
+
+	payload := strings.NewReader("method=system.token&location=ECCFF058-9506-4187-536C-EE8A39B5D79D&uuid=3BC6EA43-5551-4EB6-8D12-557AD4AD3E3E&language=zh&year=2019&token=" + token + "&server_tag=200")
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+	if err != nil {
+		panic(err)
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("User-Agent", "MGC/9.5.8 CFNetwork/1121.2.2 Darwin/19.3.0")
+	req.Header.Add("Connection", "keep-alive")
+	req.Header.Add("Accept", "*/*")
+	req.Header.Add("Accept-Language", "zh-tw")
+	req.Header.Add("Cache-Control", "no-cache")
+
+	res, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer res.Body.Close()
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+	config := Config{}
+	err = json.Unmarshal(body, &config)
+	if err != nil {
+		panic(err)
+	}
+
+	getCatMap(config)
+	getCityMap(config)
+}
+func getInfoFromShareLink(shareLink string) (tel, descZh string) {
+	fmt.Println(shareLink)
+	res, err := http.Get(shareLink)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("div[class=phone-info]").Each(func(i int, selection *goquery.Selection) {
+		tel = selection.Text()
+		fmt.Println(tel)
+	})
+
+	doc.Find("span[class=article-quote]").Each(func(i int, selection *goquery.Selection) {
+
+		descZh = selection.Text()
+	})
+
+	htmlText, _ := doc.Html()
+	ss := strings.Split(htmlText, `<div class="opening-hours-item border-bottom">`)
+	sss := strings.Split(ss[1], `<div class="price-subtitle">晚膳</div>`)
+	//sss[0] lunch open hour
+
+	ssss := strings.Split(sss[1], `<!-- ICON and TEXT -->`)
+	fmt.Println(ssss[0])
+
+	return tel, descZh
+}
+
+func getFromEnLink(shareLink string) (enName string, descEn string) {
+	fmt.Println(shareLink)
+	res, err := http.Get(shareLink)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		log.Fatalf("status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	// Load the HTML document
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	doc.Find("title").Each(func(i int, selection *goquery.Selection) {
+		enName = selection.Text()
+		fmt.Println(enName)
+	})
+
+	doc.Find("span[class=article-quote]").Each(func(i int, selection *goquery.Selection) {
+
+		descEn = selection.Text()
+	})
+
+	return enName, descEn
+}
+
+func main() {
+	getConfig()
+	fmt.Println("getConfig success")
+
+	resaurants := getResaurant()
+
+	for k, v := range resaurants {
+		cityName := citymap[k]
+		for _, resaurant := range v {
+			// Request the HTML page.
+
+			fmt.Println(resaurant.Name)
+			shareLinkZh := "https://www.mzhinan.cn/share/restaurant/" + resaurant.ID + "/zh"
+			shareLinkEn := "https://www.mzhinan.cn/share/restaurant/" + resaurant.ID + "/en"
+
+			tel, descZh := getInfoFromShareLink(shareLinkZh)
+
+			enName, descEn := getFromEnLink(shareLinkEn)
+
+			ls := strings.Split(resaurant.CoordinateAmap, ",")
+
+			finalData[cityName] = append(finalData[cityName], RequireData{
+				NameCn:  resaurant.Name,
+				NameEn:  enName,
+				Address: resaurant.Address,
+				Tel:     tel,
+				Url:     shareLinkZh,
+				Star:    scoreMap[resaurant.Star],
+				Cat:     catMap[resaurant.Cuisine],
+				DescZh:  descZh,
+				DescEn:  descEn,
+				Lat:     ls[1],
+				Long:    ls[0],
+				//OpenDiner: info.OpeningHoursDinner,
+				//OpenLunch: info.OpeningHoursLunch,
+				//Price:     info.Price,
+			})
+		}
+	}
+
+}
+
+func getResaurant() map[string][]Restaurant {
+	jsonFile, err := os.Open("data.json")
+	if err != nil {
+		panic(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+
+	resp := Response{}
+	err = json.Unmarshal(byteValue, &resp)
+	if err != nil {
+		panic(err)
+	}
+
+	cityMap := map[string][]Restaurant{}
+
+	for _, restaurant := range resp.Restaurant {
+		if _, ok := cityMap[restaurant.City]; !ok {
+			cityMap[restaurant.City] = []Restaurant{}
+		}
+		cityMap[restaurant.City] = append(cityMap[restaurant.City], restaurant)
+	}
+
+	return cityMap
+}
---
+++ models.go
@@ -0,0 +1,220 @@
+package main
+
+type RestaurantInfo struct {
+	Code       string     `json:"code"`
+	Message    string     `json:"message"`
+	Restaurant DetailInfo `json:"restaurant"`
+}
+
+type Config struct {
+	Code            string     `json:"code"`
+	Message         string     `json:"message"`
+	Token           string     `json:"token"`
+	Areas           []CatInfo  `json:"areas"`
+	APIVersion      string     `json:"api_version"`
+	Location        []Location `json:"location"`
+	CuisineCat      []CatSet   `json:"cuisine_cat"`
+	AzureBlobConfig struct {
+		AccessKeyID     string `json:"accessKeyId"`
+		AccessKeySecret string `json:"accessKeySecret"`
+		Bucket          string `json:"bucket"`
+		OuterInternet   string `json:"outerInternet"`
+		InnerInternet   string `json:"innerInternet"`
+	} `json:"azure_blob_config"`
+	AzureImgURL         string `json:"azure_img_url"`
+	AzureBucket         string `json:"azure_bucket"`
+	SwitchLocationLogin int    `json:"switch_location_login"`
+	Taste               string `json:"taste"`
+	UpgradeInfo         string `json:"upgrade_info"`
+	ShowLocationMore    int    `json:"show_location_more"`
+	AccountBg           []struct {
+		LocationID string `json:"location_id"`
+		Img        string `json:"img"`
+	} `json:"account_bg"`
+	ShowAndroidUpdate int    `json:"show_android_update"`
+	AndroidVersion    string `json:"android_version"`
+	ShowIosUpdate     int    `json:"show_ios_update"`
+	IosVersion        string `json:"ios_version"`
+	LocationMore      struct {
+		ID     string `json:"id"`
+		NameEn string `json:"name_en"`
+		NameZh string `json:"name_zh"`
+		Image  string `json:"image"`
+	} `json:"location_more"`
+	OssBucket   string  `json:"oss_bucket"`
+	Comfort     []Icon  `json:"comfort"`
+	AllComfort  Icon    `json:"all_comfort"`
+	AllSymbols  Icon    `json:"all_symbols"`
+	AllStar     Icon    `json:"all_star"`
+	AllCuisines CatInfo `json:"all_cuisines"`
+	AllAreas    CatInfo `json:"all_areas"`
+	Symbols     []Icon  `json:"symbols"`
+	Star        []struct {
+		ID    string `json:"id"`
+		Index string `json:"index"`
+		Icon  string `json:"icon"`
+		Name  string `json:"name"`
+	} `json:"star"`
+	Cuisines       []CatInfo `json:"cuisines"`
+	BaseURLImage   string    `json:"base_url_image"`
+	InvitationCode string    `json:"invitation_code"`
+	InvitationMode int       `json:"invitation_mode"`
+	ArticleCat     []struct {
+		ID       string `json:"id"`
+		Name     string `json:"name"`
+		Children []struct {
+			ID   string `json:"id"`
+			Name string `json:"name"`
+		} `json:"children"`
+	} `json:"article_cat"`
+}
+
+type DetailInfo struct {
+	ID                 string      `json:"id"`
+	Ga                 string      `json:"ga"`
+	Name               string      `json:"name"`
+	Cuisine            string      `json:"cuisine"`
+	Tel                string      `json:"tel"`
+	Star               string      `json:"star"`
+	Comfort            interface{} `json:"comfort"`
+	Website            string      `json:"website"`
+	IsRed              string      `json:"is_red"`
+	Price              Price       `json:"price"`
+	Carousel           []string    `json:"carousel"`
+	City               string      `json:"city"`
+	District           string      `json:"district"`
+	Area               string      `json:"area"`
+	Address            string      `json:"address"`
+	ShortAddress       string      `json:"short_address"`
+	CoordinateAmap     string      `json:"coordinate_amap"`
+	CoordinateOri      string      `json:"coordinate_ori"`
+	OpeningHoursLunch  []OpenHour  `json:"opening_hours_lunch"`
+	OpeningHoursDinner []OpenHour  `json:"opening_hours_dinner"`
+	Holidays           string      `json:"holidays"`
+	Desc               string      `json:"desc"`
+	Preorder           string      `json:"preorder"`
+	Refuse             string      `json:"refuse"`
+	KoubeiID           string      `json:"koubei_id"`
+	KoubeiURL          string      `json:"koubei_url"`
+	AeURL              interface{} `json:"ae_url"`
+	AeText             interface{} `json:"ae_text"`
+	Owner              string      `json:"owner"`
+	OwnerSince         string      `json:"owner_since"`
+	OwnerExp           string      `json:"owner_exp"`
+	Manager            string      `json:"manager"`
+	ManagerSince       string      `json:"manager_since"`
+	ManagerExp         string      `json:"manager_exp"`
+	Chef               string      `json:"chef"`
+	ChefSince          string      `json:"chef_since"`
+	ChefExp            string      `json:"chef_exp"`
+	Symbols            []string    `json:"symbols"`
+	Capacity           string      `json:"capacity"`
+	Nearby             []struct {
+		ID        string `json:"id"`
+		Distrance string `json:"distrance"`
+	} `json:"nearby"`
+	Articles  []interface{} `json:"articles"`
+	ShareURL  string        `json:"share_url"`
+	Set       []interface{} `json:"set"`
+	HasSet    bool          `json:"has_set"`
+	PhotoList []interface{} `json:"photo_list"`
+	Title     string        `json:"title"`
+}
+
+type Restaurant struct {
+	ID                 string      `json:"id"`
+	Name               string      `json:"name"`
+	Cuisine            string      `json:"cuisine"`
+	Star               string      `json:"star"`
+	Comfort            string      `json:"comfort"`
+	IsRed              string      `json:"is_red"`
+	DinnerMenuMin      string      `json:"dinner_menu_min"`
+	DinnerMenuMax      string      `json:"dinner_menu_max"`
+	DinnerSetMin       string      `json:"dinner_set_min"`
+	DinnerSetMax       string      `json:"dinner_set_max"`
+	LunchMenuMin       string      `json:"lunch_menu_min"`
+	LunchMenuMax       string      `json:"lunch_menu_max"`
+	LunchSetMin        string      `json:"lunch_set_min"`
+	LunchSetMax        string      `json:"lunch_set_max"`
+	CoordinateAmap     string      `json:"coordinate_amap"`
+	Address            string      `json:"address"`
+	ShortAddress       string      `json:"short_address"`
+	City               string      `json:"city"`
+	Area               string      `json:"area"`
+	Thumbnail          string      `json:"thumbnail"`
+	Symbols            []string    `json:"symbols"`
+	RelatedArticleTime interface{} `json:"related_article_time"`
+	HasSet             bool        `json:"has_set"`
+	Desc               string      `json:"desc
+"`
+}
+
+type Response struct {
+	Code       string       `json:"code"`
+	Message    string       `json:"message"`
+	Restaurant []Restaurant `json:"restaurant"`
+}
+
+type CatInfo struct {
+	ID    string `json:"id"`
+	Image string `json:"image"`
+	Name  string `json:"name"`
+}
+
+type Icon struct {
+	ID   string `json:"id"`
+	Icon string `json:"icon"`
+	Name string `json:"name"`
+}
+
+type CatSet struct {
+	ID       string    `json:"id"`
+	Name     string    `json:"name"`
+	Image    string    `json:"image"`
+	Children []CatInfo `json:"children"`
+}
+
+type Location struct {
+	ID         string `json:"id"`
+	Code       string `json:"code"`
+	NameZh     string `json:"name_zh"`
+	NameEn     string `json:"name_en"`
+	Image      string `json:"image"`
+	Index      string `json:"index"`
+	RegionCode string `json:"region_code"`
+}
+
+type RequireData struct {
+	NameCn    string
+	NameEn    string
+	Address   string
+	Tel       string
+	Url       string
+	Star      string
+	Cat       string
+	DescZh    string
+	DescEn    string
+	Lat       string
+	Long      string
+	OpenLunch string
+	OpenDiner string
+	Price     Price
+}
+
+type OpenHour struct {
+	Range string `json:"range"`
+	Open  string `json:"open"`
+	End   string `json:"end"`
+	Lo    string `json:"lo"`
+}
+
+type Price struct {
+	LunchSetMin   string `json:"lunch_set_min"`
+	LunchSetMax   string `json:"lunch_set_max"`
+	LunchMenuMin  string `json:"lunch_menu_min"`
+	LunchMenuMax  string `json:"lunch_menu_max"`
+	DinnerSetMin  string `json:"dinner_set_min"`
+	DinnerSetMax  string `json:"dinner_set_max"`
+	DinnerMenuMin string `json:"dinner_menu_min"`
+	DinnerMenuMax string `json:"dinner_menu_max"`
+}
